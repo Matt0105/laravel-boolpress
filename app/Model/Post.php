0a3b88bb555aa45d78881929f1ee453581e68a30
Matt0105 <matteo0105@gmail.com>
2022-03-01 15:40:16 +0100

    added userInfo table and relationship with user

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+        /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    
 }
