4b3e45dc592ff60cfc5ed920fa8c1c9ba322196c
Matt0105 <matteo0105@gmail.com>
2022-03-04 15:08:35 +0100

    added tags table and many to many relationship

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -24,5 +24,9 @@ class Post extends Model
     public function category() {
         return $this->belongsTo("App\Model\Category");
     }
+
+    public function tags() {
+        return $this->belongsToMany("App\Model\Tag")->withTimestamps();
+    }
     
 }
