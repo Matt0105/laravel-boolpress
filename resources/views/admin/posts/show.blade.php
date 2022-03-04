a55f1ad431d14ec0edc8a40a5d9ed449bbdb78ac
Matt0105 <matteo0105@gmail.com>
2022-03-04 16:45:11 +0100

    added tags in show page and created role table

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,6 +7,18 @@
         <h1>{{$post->title}}</h1>
         <p>{{$post->content}}</p>
         <p><em>Author: {{$post->user()->first()->name}}</em></p>
+        <p>
+            <em>Tags:</em> 
+            @foreach ($post->tags()->get() as $tag)
+                @if($loop->last) 
+                    #{{$tag->name}}
+                
+                @else 
+                    <em>#{{$tag->name}},</em>
+                @endif
+                
+            @endforeach
+        </p>
     </div>
     <a class="btn btn-dark" href="{{url()->previous()}}">< Back</a>
 </div>
