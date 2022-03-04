473521a4641d1f226d9fefeb476b1dc144089289
Matt0105 <matteo0105@gmail.com>
2022-03-04 15:17:03 +0100

    added tag list in index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -16,6 +16,7 @@
                     <th scope="col">#</th>
                     <th scope="col">Title</th>
                     <th scope="col">Category</th>
+                    <th scope="col">Tags</th>
                     <th scope="col">Content</th>
                     <th colspan=3 scope="col">Action</th>
                 </tr>
@@ -26,6 +27,14 @@
                         <th scope="row">{{$post->id}}</th>
                         <td>{{$post->title}}</td>
                         <td>{{$post->category()->first()->name}}</td>
+                        <td>
+
+                            @foreach ($post->tags()->get() as $tag)
+                                <ul style="list-style-type: none">
+                                    <li style="text-decoration: underline">#{{$tag->name}}</li>
+                                </ul>
+                            @endforeach
+                        </td>
                         <td>{{$post->content}}</td>
                         <td><a class="btn btn-primary" href="{{route('admin.posts.show', $post)}}" style="color: white">View</a></td>
                             
