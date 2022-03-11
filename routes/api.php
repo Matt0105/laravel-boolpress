739e763ec95ec63a4dd1fd0e27ee507f77007949
Matt0105 <matteo0105@gmail.com>
2022-03-11 14:42:01 +0100

    added authorization

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get("/posts/random", "Api\PostController@index");
 Route::get("/posts/allPosts", "Api\PostController@allPosts");
 Route::get("/posts/search", "Api\PostController@filter");
-Route::get("/posts/{id}", "Api\PostController@show");
+Route::get("/posts/{id}", "Api\PostController@show")->middleware("api.auth");
